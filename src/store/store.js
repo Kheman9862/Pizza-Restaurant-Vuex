f3aa3563c65cf037979ee81d0aba36077b556bfb
Kheman9862 <55815813+Kheman9862@users.noreply.github.com>
2020-02-20 22:29:25 -0500

    Update store.js

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,5 @@
 import Vue from "vue";
 import Vuex from "vuex";
-// import * as actions from "./actions";
-// import * as getters from "./getters";
-// import * as mutations from "./mutations";
 
 import menu from "./modules/menu";
 import orders from "./modules/orders";
